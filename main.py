644858bb0f133a34568bc9a7dcf7ba59158696ef
OrangeMintz <Nickzgacus@gmail.com>
2025-07-31 20:04:06 +0800

    minor changesx

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fitgirl_api import FitGirlAPI
 
 fitGirl = FitGirlAPI()
-upcoming_release = fitGirl.upcoming_release()
+# upcoming_release = fitGirl.upcoming_release()
 new_release = fitGirl.new_release()
